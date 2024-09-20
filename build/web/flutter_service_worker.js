292f64560a169eb99887969713b3219968cc628a
Quang Thiện <108052745+devlow03@users.noreply.github.com>
2024-09-20 19:37:16 +0700

    update web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -47,7 +47,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "747cee7bb8edc62c83f940b093e8f6fb
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "3ad1e887c10ea7e7e5155c1cec4008c9",
+"flutter_bootstrap.js": "a7a3ae125698198f4e18dc6dd0c2682f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
