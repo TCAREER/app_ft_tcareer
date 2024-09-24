cabbec9e8276f3ef70ab00b4a35a1050a7bbbf6a
Quang Thiện <108052745+devlow03@users.noreply.github.com>
2024-09-24 17:48:26 +0700

    feat: logo, splash

--- lib/src/modules/posts/presentation/pages/home_page.dart
+++ lib/src/modules/posts/presentation/pages/home_page.dart
@@ -152,6 +152,7 @@ class _HomePageState extends ConsumerState<HomePage> {
       leading: Image.asset(
         "assets/images/logo.png",
         width: 100,
+        height: 100,
         fit: BoxFit.cover,
       ),
       leadingWidth: 120,
