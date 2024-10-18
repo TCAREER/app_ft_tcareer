45a362ff83d7612be83a32716f8c97cb8ef0ca29
devlow03 <nth4356@gmail.com>
2024-10-18 22:48:36 +0700

    feat: firebase otp

--- lib/src/services/twilio/twilio_service.dart
+++ lib/src/services/twilio/twilio_service.dart
@@ -4,15 +4,14 @@ import 'package:twilio_flutter/twilio_flutter.dart';
 
 class TwilioService {
   final TwilioFlutter twilioFlutter = TwilioFlutter(
-    accountSid:
-        "ACb84a4badc61503296b64b8b032db496d", // replace with Account SID
+    accountSid: "", // replace with Account SID
     authToken: AppConstants.twilioAuthToken, // replace with Auth Token
     twilioNumber: "+84", // replace with Twilio Number(With country code)
   );
 
   Future<TwilioResponse> sendVerificationCode(String phoneNumber) async {
     TwilioResponse response = await twilioFlutter.sendVerificationCode(
-        verificationServiceId: "VAa60e455052ed95e037baaae39a7f2fcb",
+        verificationServiceId: "",
         recipient: phoneNumber,
         verificationChannel: VerificationChannel.SMS);
     return response;
@@ -21,9 +20,7 @@ class TwilioService {
   Future<TwilioResponse> verifyCode(
       {required String phoneNumber, required String code}) async {
     TwilioResponse response = await twilioFlutter.verifyCode(
-        verificationServiceId: "VAa60e455052ed95e037baaae39a7f2fcb",
-        recipient: phoneNumber,
-        code: code);
+        verificationServiceId: "", recipient: phoneNumber, code: code);
     return response;
   }
 }
