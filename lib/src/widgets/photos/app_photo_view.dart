4ea0c0edbe401e525ab697bf04bf21840bb24e73
devlow03 <nth4356@gmail.com>
2024-10-21 12:11:01 +0700

    feat: download image in photoview

--- lib/src/widgets/photos/app_photo_view.dart
+++ lib/src/widgets/photos/app_photo_view.dart
@@ -2,16 +2,23 @@ import 'dart:io';
 import 'package:app_tcareer/src/features/index/index_controller.dart';
 import 'package:app_tcareer/src/features/posts/presentation/posts_provider.dart';
 import 'package:app_tcareer/src/features/posts/presentation/widgets/engagement_widget.dart';
+import 'package:app_tcareer/src/utils/snackbar_utils.dart';
 import 'package:app_tcareer/src/widgets/photos/app_photo_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class AppPhotoView extends StatefulWidget {
   final AppPhotoModel data;
@@ -41,6 +48,45 @@ class _AppPhotoViewState extends State<AppPhotoView> {
     });
   }
 
+  final Dio dio = Dio();
+  Future<void> downLoadImage(String imageUrl) async {
+    try {
+      final plugin = DeviceInfoPlugin();
+      final android = await plugin.androidInfo;
+
+      var status = android.version.sdkInt < 33
+          ? await Permission.storage.request()
+          : PermissionStatus.granted;
+      print(">>>>>>>>status: $status");
+      dio.interceptors.add(PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+          responseHeader: false,
+          error: true,
+          compact: true,
+          maxWidth: 90));
+      if (status.isGranted) {
+        Directory? appDocDir = await getExternalStorageDirectory();
+        String savePath =
+            '${appDocDir!.path}/Pictures/${DateTime.now().millisecondsSinceEpoch}.jpg';
+        print(">>>>>>>>>>>savePath: $savePath");
+        final response = await dio.download(imageUrl, savePath);
+        if (response.statusCode == 200) {
+          final result = await ImageGallerySaver.saveFile(savePath);
+
+          if (result != null) {
+            showSnackBar("Tải ảnh về thành công");
+          }
+        } else {
+          showSnackBarError("Tài ảnh về thất bại");
+        }
+      }
+    } catch (e) {
+      print(">>>>>>>$e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return PopScope(
@@ -55,17 +101,26 @@ class _AppPhotoViewState extends State<AppPhotoView> {
             backgroundColor: Colors.black,
             centerTitle: true,
             leading: IconButton(
-              icon: Icon(
+              icon: const Icon(
                 Icons.close,
                 color: Colors.white,
               ),
               onPressed: () => context.pop(),
             ),
             automaticallyImplyLeading: false,
-            titleTextStyle: TextStyle(color: Colors.white),
+            titleTextStyle: const TextStyle(color: Colors.white),
             title: Visibility(
                 visible: widget.data.images.length > 1,
                 child: Text("${index + 1}/${widget.data.images.length}")),
+            actions: [
+              IconButton(
+                  onPressed: () async =>
+                      await downLoadImage(widget.data.images[index]),
+                  icon: Icon(
+                    Icons.download,
+                    color: Colors.white,
+                  ))
+            ],
           ),
           body: Stack(
             alignment: Alignment.bottomCenter,
